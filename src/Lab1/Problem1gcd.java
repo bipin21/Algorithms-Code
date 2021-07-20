77d26672644c3c6156e1083b247fd36ad726b587
Bipin Karki <bipinkarki172@gmail.com>
2021-07-20 14:47:14 -0500

    GCD with brute force

--- src/Lab1/Problem1gcd.java
+++ src/Lab1/Problem1gcd.java
@@ -1,11 +1,34 @@
 package Lab1;
 
 public class Problem1gcd {
-    int gcd(int m, int n) {
-        return 0;
+    static int gcd(int m, int n) {
+        int temp = m;
+        if (m > n) {
+            m = n;
+            n = temp;
+        }
+        if (n % m == 0) return m;
+        int range = m / 2;
+        int max = -1;
+        for (int i = 2; i < range; i++) {
+            if (m % i == 0) {
+                if (n % i == 0) {
+                    max = i;
+                }
+            }
+        }
+        return max;
     }
 
     public static void main(String[] args) {
 
+        System.out.println("GCD is : " + gcd(24, 20));
+        System.out.println("GCD is : " + gcd(20, 24));
+
+        System.out.println("GCD is : " + gcd(98, 56));
+        System.out.println("GCD is : " + gcd(56, 98));
+
+        System.out.println("GCD is : " + gcd(13, 17));
+        System.out.println("GCD is : " + gcd(13, 15));
     }
 }
